e973159f3fcf83246e603c017412d91d665e2536
gpay <gpaytakov@gmail.com>
2022-05-18 23:57:30 -0500

    like-seeds.js updated

--- seeds/like-seeds.js
+++ seeds/like-seeds.js
@@ -3,191 +3,191 @@ const { ThumbsUp } = require("../models");
 const likedata = [
   {
     user_id: 9,
-    quotes_id: 19,
+    quote_id: 19,
   },
   {
     user_id: 1,
-    quotes_id: 8,
+    quote_id: 8,
   },
   {
     user_id: 8,
-    quotes_id: 12,
+    quote_id: 12,
   },
   {
     user_id: 8,
-    quotes_id: 19,
+    quote_id: 19,
   },
   {
     user_id: 9,
-    quotes_id: 3,
+    quote_id: 3,
   },
   {
     user_id: 3,
-    quotes_id: 16,
+    quote_id: 16,
   },
   {
     user_id: 4,
-    quotes_id: 7,
+    quote_id: 7,
   },
   {
     user_id: 10,
-    quotes_id: 7,
+    quote_id: 7,
   },
   {
     user_id: 3,
-    quotes_id: 18,
+    quote_id: 18,
   },
   {
     user_id: 9,
-    quotes_id: 16,
+    quote_id: 16,
   },
   {
     user_id: 3,
-    quotes_id: 17,
+    quote_id: 17,
   },
   {
     user_id: 10,
-    quotes_id: 2,
+    quote_id: 2,
   },
   {
     user_id: 6,
-    quotes_id: 10,
+    quote_id: 10,
   },
   {
     user_id: 5,
-    quotes_id: 11,
+    quote_id: 11,
   },
   {
     user_id: 6,
-    quotes_id: 1,
+    quote_id: 1,
   },
   {
     user_id: 9,
-    quotes_id: 18,
+    quote_id: 18,
   },
   {
     user_id: 6,
-    quotes_id: 15,
+    quote_id: 15,
   },
   {
     user_id: 6,
-    quotes_id: 7,
+    quote_id: 7,
   },
   {
     user_id: 6,
-    quotes_id: 4,
+    quote_id: 4,
   },
   {
     user_id: 1,
-    quotes_id: 16,
+    quote_id: 16,
   },
   {
     user_id: 10,
-    quotes_id: 18,
+    quote_id: 18,
   },
   {
     user_id: 4,
-    quotes_id: 10,
+    quote_id: 10,
   },
   {
     user_id: 10,
-    quotes_id: 5,
+    quote_id: 5,
   },
   {
     user_id: 5,
-    quotes_id: 16,
+    quote_id: 16,
   },
   {
     user_id: 6,
-    quotes_id: 17,
+    quote_id: 17,
   },
   {
     user_id: 1,
-    quotes_id: 15,
+    quote_id: 15,
   },
   {
     user_id: 7,
-    quotes_id: 13,
+    quote_id: 13,
   },
   {
     user_id: 6,
-    quotes_id: 3,
+    quote_id: 3,
   },
   {
     user_id: 6,
-    quotes_id: 13,
+    quote_id: 13,
   },
   {
     user_id: 7,
-    quotes_id: 1,
+    quote_id: 1,
   },
   {
     user_id: 4,
-    quotes_id: 15,
+    quote_id: 15,
   },
   {
     user_id: 2,
-    quotes_id: 18,
+    quote_id: 18,
   },
   {
     user_id: 9,
-    quotes_id: 10,
+    quote_id: 10,
   },
   {
     user_id: 10,
-    quotes_id: 15,
+    quote_id: 15,
   },
   {
     user_id: 8,
-    quotes_id: 1,
+    quote_id: 1,
   },
   {
     user_id: 10,
-    quotes_id: 8,
+    quote_id: 8,
   },
   {
     user_id: 2,
-    quotes_id: 13,
+    quote_id: 13,
   },
   {
     user_id: 9,
-    quotes_id: 20,
+    quote_id: 20,
   },
   {
     user_id: 1,
-    quotes_id: 17,
+    quote_id: 17,
   },
   {
     user_id: 10,
-    quotes_id: 9,
+    quote_id: 9,
   },
   {
     user_id: 10,
-    quotes_id: 3,
+    quote_id: 3,
   },
   {
     user_id: 5,
-    quotes_id: 6,
+    quote_id: 6,
   },
   {
     user_id: 6,
-    quotes_id: 12,
+    quote_id: 12,
   },
   {
     user_id: 5,
-    quotes_id: 2,
+    quote_id: 2,
   },
   {
     user_id: 6,
-    quotes_id: 14,
+    quote_id: 14,
   },
   {
     user_id: 8,
-    quotes_id: 18,
+    quote_id: 18,
   },
   {
     user_id: 3,
-    quotes_id: 4,
+    quote_id: 4,
   },
 ];
 

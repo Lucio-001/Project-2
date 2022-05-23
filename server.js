f55fb0e871930e1e1e7470cd227900dc5b0be2b3
Govnor Payne <govnorpayne@gmail.com>
2022-05-22 23:14:26 -0500

    fixed router

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const exphbs = require("express-handlebars");
 // const Handlebars = require("handlebars");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
 
 const sequelize = require("./config/connection.js");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
